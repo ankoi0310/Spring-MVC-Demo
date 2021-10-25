d7bc4d98468b46398635ebf94b6c2e0924d4b607
Huynh Van Huu An <19130003@st.hcmuaf.edu.vn>
2021-10-25 10:50:40 +0700

    @RequestParam to get request parameter

---
+++ src/main/java/com/koi/springdemo/mvc/ConflictController.java
@@ -0,0 +1,13 @@
+package com.koi.springdemo.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ConflictController {
+
+    @RequestMapping("/hello")
+    public String displayForm() {
+        return "conflict";
+    }
+}
--- src/main/java/com/koi/springdemo/mvc/HelloWorldController.java
+++ src/main/java/com/koi/springdemo/mvc/HelloWorldController.java
@@ -3,6 +3,7 @@ package com.koi.springdemo.mvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -20,8 +21,8 @@ public class HelloWorldController {
     }
 
     @RequestMapping("/processFormVer2")
-    public String sayHello(HttpServletRequest request, Model model) {
-        String name = request.getParameter("studentName");
+    public String sayHello(@RequestParam("studentName") String name, Model model) {
+        // String name = request.getParameter("studentName");
         name = name.toUpperCase();
 
         String result = "Hello " + name;
